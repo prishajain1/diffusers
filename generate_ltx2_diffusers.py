655d0a958b270f0b099d9e12fbf38660596b5e5d
Prisha Jain <prishajain@google.com>
2026-03-05 08:59:38 +0530

    generate_ltx2 diffusers script

--- generate_ltx2_diffusers.py
+++ generate_ltx2_diffusers.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 import torch
 from diffusers.pipelines.ltx2.pipeline_ltx2 import LTX2Pipeline
 from diffusers.utils import export_to_video
